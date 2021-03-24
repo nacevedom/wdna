35f4dfae7197820bbfc556a59634da34cdbd640f
Nico <acevedom.nico@gmail.com>
2021-03-24 13:09:40 +0100

    prueba2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadsdf.
-        </p>
+        <h1>Hola nico</h1>
+        <h2>PRUEBAS</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
