89ef1c1b6c57b6f7f28ddd90dde1107dff99df70
Nico <acevedom.nico@gmail.com>
2021-03-24 14:37:34 +0100

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import StyleNavbar from "../assets/styles/navbar.css";
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      Navbar
+    </a>
+    <ul>
+      <li>Temperaturas</li>
+      <li>Gráficos</li>
+      <li>ejemplo</li>
+      <li>EJemplo</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
