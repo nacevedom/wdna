868c731cb1ad1817f508660f12629de8040f74cb
Nico <acevedom.nico@gmail.com>
2021-05-28 09:27:39 +0200

    data json zonas

--- src/components/ColumnChart.jsx
+++ src/components/ColumnChart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import mapboxgl from "mapbox-gl";
 import axios from "axios";
 import "../App.css";
-
+import moment from "moment";
 mapboxgl.accessToken =
   "pk.eyJ1IjoibmFjZXZlZG9tIiwiYSI6ImNrb3d1cGFuajA5dXoydm1wdDZsbDEzMjAifQ.U9HqbhNdNMc0PJuI6dygaQ";
 
@@ -14,7 +14,7 @@ export const Fetchdata = async () => {
   this.setState({
     dataTemp: data.data,
   });
-  console.log(data.data);
+
   const data2 = await axios.get(
     "http://meteoclim.meteoclim.com/apptrack/public/sensor/weatherstation/listallwithinfo?id=1"
   );
@@ -30,45 +30,40 @@ export default class App extends React.Component {
     this.state = {
       _isMounted: false,
       temperatures: [],
-      prueba: [
-        {
-          zonas: [
-            {
-              zona_1: {
-                id: "aemet-9562X",
-              },
-            },
-            // {
-            //   zona_2: {
-            //     id: "aemet-8520X",
-            //   },
-            // },
-            // {
-            //   zona_4: {
-            //     id: "aemet-8523X",
-            //   },
-            // },
-            // {
-            {
-              zona_5: [
-                {
-                  id: "aemet-8489X",
-                },
-                { id: "aemet-9563X" },
-              ],
-            },
+      datajson: require("./zonas.json"),
 
-            // {
-            //   zona_20: [
-            //     { id: "aemet-8050X" },
-            //     { id: "aemet-8058X" },
-            //     { id: "aemet-8058Y" },
-            //     { id: "aemet-8072Y" },
-            //   ],
-            // },
-          ],
-        },
-      ],
+      prueba: {
+        zonas: [
+          {
+            id: "aemet-9562X",
+            zona: "zona_1",
+          },
+          // {
+          //   zona_2: {
+          //     id: "aemet-8520X",
+          //   },
+          // },
+          // {
+          //   zona_4: {
+          //     id: "aemet-8523X",
+          //   },
+          // },
+          // {
+          {
+            id: ["aemet-8489X", "aemet-9563X"],
+            zona: "zona_5",
+          },
+
+          // {
+          //   zona_20: [
+          //     { id: "aemet-8050X" },
+          //     { id: "aemet-8058X" },
+          //     { id: "aemet-8058Y" },
+          //     { id: "aemet-8072Y" },
+          //   ],
+          // },
+        ],
+      },
 
       dataCord: [],
       dataTemp: [],
@@ -132,9 +127,18 @@ export default class App extends React.Component {
     this._isMounted = false;
   }
   insertData = async () => {
+    var dateFormat = require("dateformat");
+    var date = new Date();
+
+    var subs = moment().subtract(3, "hours");
+    var minute = subs.minutes();
+
+    var startdate = subs.subtract(minute, "minutes").format("YYYY-MM-DDTHH:mm");
+
+    var enddate = dateFormat(date, "isoDateTime");
     try {
       const data = await axios.get(
-        "http://meteoclim.meteoclim.com/apptrack/public/sensor/weatherstation/owner/historical?id=1&start=2021-03-30%2013:00:00&end=2021-03-30%2013:00:00"
+        `http://meteoclim.meteoclim.com/apptrack/public/sensor/weatherstation/owner/historical?id=1&start=${startdate}&end=${enddate}`
       );
       if (this._isMounted) {
         this.setState({
@@ -157,14 +161,13 @@ export default class App extends React.Component {
           dataCord: data2.data,
         });
       }
-      console.log(data2);
     } catch (e) {
       console.log(e);
     }
     this.insertData();
   };
   insertResult = async () => {
-    const { dataTemp, dataCord } = this.state;
+    const { dataTemp, dataCord, prueba, datajson } = this.state;
 
     var result = [];
     var temp = [];
@@ -189,52 +192,51 @@ export default class App extends React.Component {
       temp.push(n.temperature);
     });
 
-    var zonas = {
-      "aemet-8337X": "zona 15",
-      "aemet-7244X": "zona 29",
-      "aemet-7247X": "zona 27",
-      "aemet-7261X": "zona 30",
-      "aemet-8005X": "zona 21",
-      "aemet-8008Y": "zona 21",
-      "aemet-8013X": "zona 28",
-      "aemet-8018X": "zona 28",
-      "aemet-8019": "zona 26",
-      "aemet-8025": " zona 26",
-      "aemet-8050X": "zona 20",
-      "aemet-8057C": "zona 19",
-      "aemet-8058X": "zona 20",
-      "aemet-8058Y": "zona 20",
-      "aemet-8059C": "zona 22",
-      "aemet-8072Y": "zona 20",
-      "aemet-8193E": "zona 14",
-      "aemet-8203O": "zona 14",
-      "aemet-8270X": "zona 15",
-      "aemet-8283X": "zona 18",
-      "aemet-8293X": "zona 17",
-      "aemet-8300X": "zona 17",
-      "aemet-8309X": "zona 11",
-      "aemet-8319X": "zona 15",
-      "aemet-8325X": "zona 13",
-      "aemet-8381X": "zona 10",
-      "aemet-8395X": "zona 10",
-      "aemet-8409X": "zona 12",
-      "aemet-8414A": "zona 13",
-      "aemet-8416X": "zona 13",
-      "aemet-8416Y": "zona 13",
-      "aemet-8446Y": "zona 9",
-      "aemet-8472A": "zona 6",
-      "aemet-8489X": "zona 5",
-      "aemet-8492X": "zona 7",
-      "aemet-8500A": "zona 8",
-      "aemet-8503Y": "zona 8",
-      "aemet-8520X": "zona 2",
-      "aemet-8523X": "zona 4",
-      "aemet-9562X": "zona 1",
-      "aemet-9563X": "zona 5",
-      "aemet-8439X": "zona 7",
-    };
+    // var zonas = {
+    //   "aemet-8337X": "zona 15",
+    //   "aemet-7244X": "zona 29",
+    //   "aemet-7247X": "zona 27",
+    //   "aemet-7261X": "zona 30",
+    //   "aemet-8005X": "zona 21",
+    //   "aemet-8008Y": "zona 21",
+    //   "aemet-8013X": "zona 28",
+    //   "aemet-8018X": "zona 28",
+    //   "aemet-8019": "zona 26",
+    //   "aemet-8025": " zona 26",
+    //   "aemet-8050X": "zona 20",
+    //   "aemet-8057C": "zona 19",
+    //   "aemet-8058X": "zona 20",
+    //   "aemet-8058Y": "zona 20",
+    //   "aemet-8059C": "zona 22",
+    //   "aemet-8072Y": "zona 20",
+    //   "aemet-8193E": "zona 14",
+    //   "aemet-8203O": "zona 14",
+    //   "aemet-8270X": "zona 15",
+    //   "aemet-8283X": "zona 18",
+    //   "aemet-8293X": "zona 17",
+    //   "aemet-8300X": "zona 17",
+    //   "aemet-8309X": "zona 11",
+    //   "aemet-8319X": "zona 15",
+    //   "aemet-8325X": "zona 13",
+    //   "aemet-8381X": "zona 10",
+    //   "aemet-8395X": "zona 10",
+    //   "aemet-8409X": "zona 12",
+    //   "aemet-8414A": "zona 13",
+    //   "aemet-8416X": "zona 13",
+    //   "aemet-8416Y": "zona 13",
+    //   "aemet-8446Y": "zona 9",
+    //   "aemet-8472A": "zona 6",
+    //   "aemet-8489X": "zona 5",
+    //   "aemet-8492X": "zona 7",
+    //   "aemet-8500A": "zona 8",
+    //   "aemet-8503Y": "zona 8",
+    //   "aemet-8520X": "zona 2",
+    //   "aemet-8523X": "zona 4",
+    //   "aemet-9562X": "zona 1",
+    //   "aemet-9563X": "zona 5",
+    //   "aemet-8439X": "zona 7",
+    // };
 
-    console.log(this.state.prueba);
     dataCord.forEach((item) => {
       if (item.state === "Comunidad Valenciana") {
         result.push({
@@ -245,9 +247,11 @@ export default class App extends React.Component {
             temp: temp[i++],
             id: item.unique_id,
             zona: (function getzone() {
-              for (var clave in zonas) {
-                if (item.unique_id === clave) {
-                  return zonas[clave];
+              console.log(datajson.data);
+              for (i in datajson.data) {
+                for (let j = 0; j < datajson.data.length; j++) {
+                  if (datajson.data[i]) {
+                  }
                 }
               }
             })(),
@@ -273,6 +277,7 @@ export default class App extends React.Component {
   };
   insertmap = (result) => {
     const { lng, lat, zoom, prueba } = this.state;
+
     const map = new mapboxgl.Map({
       container: this.mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
@@ -294,13 +299,13 @@ export default class App extends React.Component {
       var el = document.createElement("div");
       el.className = "marker";
       el.style.backgroundColor = (function colortemp() {
-        prueba.forEach((item) => {
-          // for (let i = 0; i < item.zonas.length; i++) {
-          //   if (item.zonas[i] === marker.properties.) {
-          //   }
-          // }
-          // console.log(item.zonas[0]);
-        });
+        // prueba.forEach((item) => {
+        // for (let i = 0; i < item.zonas.length; i++) {
+        //   if (item.zonas[i] === marker.properties.) {
+        //   }
+        // }
+        // console.log(item.zonas[0]);
+        // });
         if (marker.properties.id) {
         }
         if (marker.properties.temp < 15) {
